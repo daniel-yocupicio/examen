3a9c3f217201ff5bafc19f3eb0b694dc9912a9bd
unknown <dag_98@hotmail.com>
2022-08-26 17:48:40 -0500

    add first header
    the header was added but without the functional menu

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require("express");
-const { error404Handler, errorHandler } = require('./middleware');
+const {error404Handler, errorHandler} = require('./middleware');
+var path = require('path');
+var routes = require('./routes');
 
 var app = express();
 
 app.use(express.json());
-
+app.use(routes);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+app.use(express.static('public'))
 app.use(error404Handler);
 app.use(errorHandler);
 
